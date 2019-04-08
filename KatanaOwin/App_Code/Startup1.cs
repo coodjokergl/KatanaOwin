a85a57c0b9734850f7948b246ef32a5383d4ebe0
龚磊 <Gongl01@mingyuanyun.com>
2019-04-08 20:14:21 +0800

    添加项目文件。

---
+++ KatanaOwin/App_Code/Startup1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup1))]
+
+public class Startup1
+{
+    public void Configuration(IAppBuilder app)
+    {
+        // 有关如何配置应用程序的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkID=316888
+    }
+}
